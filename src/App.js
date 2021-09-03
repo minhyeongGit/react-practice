ce525daedf673adda5e14d7177a5e52039860c16
minhyeongGit <9cd5c2@gmail.com>
2021-09-04 01:14:49 +0900

    14강 component까지

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import TOC from "./components/TOC";
+import Content from "./components/Content";
+import Subject from "./components/Subject";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello, React!!!
+        <Subject title="WEB" sub="world wide web!"></Subject>
+        <Subject title="React" sub="For UI"></Subject>
+        <Subject></Subject>
+        <TOC></TOC>
+        <Content title="HTML" desc="HTML is HyperText Markup Language."> </Content>
       </div>
     );
   }
